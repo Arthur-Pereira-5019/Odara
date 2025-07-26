b256c633ccdd093de2c9fece87e6442467fe12be
Arthur-Pereira <60709850+Arthur-Pereira-5019@users.noreply.github.com>
2025-07-25 21:32:14 -0300

    Started an animation system

--- main.cpp
+++ main.cpp
@@ -11,24 +11,51 @@ extern "C"
 
 using namespace std;
 
+
+int expectedFPS = 60;
 class Animation
 {
 public:
     int size;
     int frames;
-    int currentFrame;
+    int currentFrame = 0;
     int posX;
     int posY;
-    int speed;
+    int speed; //MEASURED IN FPS
+    int totalSize;
+    int elapsedTime = 0;
     Texture2D atlas;
+    Rectangle sheetSize;
+    
 
-    Animation(int size, int frames, int posX, int posY, int speed, string texturePath)
+    Animation(int size, int frames, int posX, int posY, int speed, string texturePath, int totalSize)
     {
         this->size = size;
         this->frames = frames;
         this->posX = posX;
         this->posY = posY;
+        this->totalSize = totalSize;
+        this->speed = speed;
         atlas = LoadTexture(texturePath.c_str());
+        this->sheetSize = {0,0,totalSize,totalSize};
+
+    }
+    Rectangle calculateRectangle() {
+        Rectangle frame = {posX+currentFrame*size,posY*size,size,size};
+        return frame;
+    }
+
+    void render(int posX, int posY) {
+        elapsedTime++;
+        if(elapsedTime > expectedFPS/speed) {
+            currentFrame++;
+            elapsedTime = 0;
+        }
+        
+        DrawTexturePro(atlas,calculateRectangle(),{posX,posY,size,size},{0,0},0,WHITE);
+        if(currentFrame == frames) {
+                currentFrame = 0;
+        } 
     }
 };
 
@@ -38,6 +65,7 @@ public:
     int speed;
     float currentSpeed;
     float movementAngle;
+    int movs = 0;
     int playerX = 400;
     int playerY = 300;
     Animation *animation;
@@ -45,56 +73,22 @@ public:
 
     Player(string texturePath, int speed, int playerX, int playerY)
     {
-        animation = new Animation(32, 4, 0, 1, 1, texturePath);
+        animation = new Animation(32, 5, 0, 1, 5, texturePath,640);
         this->speed = speed;
         this->playerX = playerX;
-        this->playerY = playerY;
+        this->playerY = playerY; 
     }
-};
 
-int main(int argc, char const *argv[])
-{
-    cout << "Hello, World!" << endl;
-    InitWindow(800, 600, "Raylib Window");
-
-    Texture2D atlas;
-    SetTargetFPS(60);
-    atlas = LoadTexture("assets/zepelintra.png");
-    int speed = 5;
-    int currentSpeed = 5;
-    float movementAngle = 0;
-    int movs = 0;
-    int playerX = 400;
-    int playerY = 300;
-
-    while (!WindowShouldClose())
-    {
+    void tick() {
         currentSpeed = speed;
-        if (IsKeyDown(KEY_RIGHT))
-        {
-            movementAngle += 0;
-            movs += 1;
-        }
-        if (IsKeyDown(KEY_LEFT))
-        {
-            movementAngle += PI;
-            movs += 1;
-        }
-        if (IsKeyDown(KEY_UP))
-        {
-            movementAngle += PI / 2;
-            movs += 1;
-        }
-        if (IsKeyDown(KEY_DOWN))
-        {
-            movementAngle += 3 * PI / 2;
-            movs += 1;
-            if (IsKeyDown(KEY_RIGHT))
-            {
-                movementAngle += 2 * PI;
-            }
-        }
+        move();
+    }
 
+    void render() {
+        animation->render(playerX,playerY);
+    }
+
+    void move() {
         if (movs > 0)
         {
             movementAngle = movementAngle / movs;
@@ -104,15 +98,55 @@ int main(int argc, char const *argv[])
 
             movs = 0;
         }
+        movementAngle = 0;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    cout << "Hello, World!" << endl;
+    InitWindow(800, 600, "Raylib Window");
+
+    Texture2D atlas;
+    SetTargetFPS(expectedFPS);
+    
+    Player *player = new Player("assets/zepelintra.png",5,400,300);
+
+    while (!WindowShouldClose())
+    {
+        if (IsKeyDown(KEY_RIGHT))
+        {
+            player->movs += 1;
+        }
+        if (IsKeyDown(KEY_LEFT))
+        {
+            player->movementAngle += PI;
+            player->movs += 1;
+
+        }
+        if (IsKeyDown(KEY_UP))
+        {
+            player->movementAngle += PI/2;
+            player->movs += 1;
+        }
+        if (IsKeyDown(KEY_DOWN))
+        {
+            player->movementAngle += 3 * PI / 2;
+            player->movs += 1;
+            if (IsKeyDown(KEY_RIGHT))
+            {
+                player->movementAngle += PI*2;
+            }
+        }
+        player->tick();
 
         BeginDrawing();
 
         ClearBackground((Color){0, 0, 0, 255});
-        DrawText(to_string(movementAngle).c_str(), 100, 50, 20, WHITE);
-        DrawTexture(atlas, playerX, playerY, WHITE);
+        player->render();
 
         EndDrawing();
-        movementAngle = 0;
+        
     }
 
     CloseWindow();
